98a0c76610b1906d6f165c993a9c498787eaed56
Irvin Homem <irvinhomem@gmail.com>
2017-03-10 23:55:29 +0100

    Initial commit

---
+++ pyadbtest.py
@@ -0,0 +1,20 @@
+
+import logging
+
+class PyAdb(object):
+
+    def __init__(self):
+        # Configure Logging
+        logging.basicConfig(level=logging.INFO)
+        # logging.basicConfig(level=logging.WARNING)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        # self.logger.setLevel(logging.DEBUG)
+        # self.logger.setLevel(logging.WARNING)
+
+
+    def adb_start(self):
+
+
+    def adb_list_devices(self):
+        
