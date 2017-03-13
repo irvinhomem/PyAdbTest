d90d1fed6fb79608d5879604ce989272d9a4a13d
Irvin Homem <irvinhomem@gmail.com>
2017-03-13 02:03:00 +0100

    Backing up ...

--- pyadbtest.py
+++ pyadbtest.py
@@ -1,5 +1,10 @@
 
 import logging
+import subprocess
+import os
+import platform
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+
 
 class PyAdb(object):
 
@@ -12,9 +17,38 @@ class PyAdb(object):
         # self.logger.setLevel(logging.DEBUG)
         # self.logger.setLevel(logging.WARNING)
 
+    def find_adb_location(self):
+        # Check OS
+        os_name = os.name
+        # Check platform
+        platform.system()
+        platform.platform()
+        platform.release()
+        platform.version()
+        # Check possible locations irrespective of OS
+        # Get user name
+
+
+        # Check if they match
+
+        # If they don't check config file
+
+        # Store ADB runtime config location
+
 
     def adb_start(self):
+        cmd = ['adb', 'root']
+        start_output = subprocess.call(cmd)
+        self.logger.info("Start: %s" % str(start_output))
 
 
     def adb_list_devices(self):
+        cmd = ['adb', 'devices']
+        subprocess.call(cmd)
+
+adb_proc = PyAdb()
+adb_proc.adb_start()
+adb_proc.adb_list_devices()
+
+
 
