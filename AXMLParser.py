ecade6195249cb34e959ac113c5cbecb2be79614
Irvin Homem <irvinhomem@gmail.com>
2017-03-16 14:17:41 +0100

    Added structure for AXMLParser Class ... incomplete, perhaps for future use

---
+++ AXMLParser.py
@@ -0,0 +1,27 @@
+
+
+class AXMLParser(object):
+
+    __schema = ''
+
+    def __init__(self):
+        return
+
+
+
+class AXML_Element(object):
+    __type = ''
+    __attributes = []
+    __child_elements = []
+    __parent = ''
+
+    def __init__(self):
+        return
+
+class AXML_Attribute(object):
+    __name = ''
+    __value = ''
+    __parent = ''
+
+    def __init__(self):
+        return
